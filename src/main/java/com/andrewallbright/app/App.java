4f38bf1026d64d76ba0d28bbf87020b3eb01295b
Andrew's 13" 512GB i5 <aallbrig@Andrewss-MacBook-Pro.local>
2017-07-11 03:09:29 -0400

    Add in dependencies (Maven) and create basic "analyze data" Java program.

--- src/main/java/com/andrewallbright/app/App.java
+++ src/main/java/com/andrewallbright/app/App.java
@@ -1,13 +1,84 @@
 package com.andrewallbright.app;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import org.apache.commons.cli.*;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.*;
+
+
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static String humanReadableSeconds (long seconds) {
+        return String.format("%02d hours %02d minutes %02d seconds", seconds / 3600, (seconds % 3600) / 60, (seconds % 60));
+    }
+    public static void main( String[] args ) throws IOException, InvalidFormatException, ParseException {
+        System.out.println("Program Start");
+        Instant startTime = new Date().toInstant();
+
+        DataFormatter formatter = new DataFormatter();
+        Options options = new Options();
+        CommandLineParser parser = new DefaultParser();
+        Set<String> uniqueAgentNames = new HashSet<>();
+        Set<String> uniqueAgentIds = new HashSet<>();
+        Duration programDuration;
+        int ignoredRows = 0;
+        int totalRowsProcessed = 0;
+
+        options.addOption("i", true, "Input file");
+        options.addOption("o", false, "Output file");
+        CommandLine cmd = parser.parse(options, args);
+
+        if (cmd.hasOption("i")) {
+            Instant wbOpenStartTime = new Date().toInstant();
+            Workbook wb = WorkbookFactory.create(new File(cmd.getOptionValue("i")));
+            Instant wbOpenComplete = new Date().toInstant();
+            long workbookOpenTimeInSeconds = Duration.between(wbOpenStartTime, wbOpenComplete).getSeconds();
+            Sheet sheet1 = wb.getSheetAt(0);
+            totalRowsProcessed = sheet1.getPhysicalNumberOfRows();
+
+            for (Row row : sheet1) {
+                Cell columnA = row.getCell(0);  // agent id
+                Cell columnH = row.getCell(4);  // agent name
+                Cell columnI = row.getCell(5);  // agent comments
+                Cell columnB = row.getCell(5);  // maybe overflow comments
+                String colAVal = formatter.formatCellValue(columnA);
+                String colHVal = formatter.formatCellValue(columnH);
+                String colIVal = formatter.formatCellValue(columnI);
+                String colBVal = formatter.formatCellValue(columnB);
+                System.out.println("row " + row.getRowNum());
+                System.out.println(
+                    "    "
+                    + (!colAVal.contentEquals("") ? ", col A - " + colAVal : "")
+                    + (!colHVal.contentEquals("") ? ", col H - " + colHVal : "")
+                    + (!colIVal.contentEquals("") ? ", col I - " + colIVal : "")
+                    + (!colBVal.contentEquals("") ? ", col B - " + colBVal : "")
+                );
+                if (colAVal == "" && colHVal == "") {
+                    ignoredRows += 1;
+                    System.out.println("...ignoring row");
+                } else if (!(colAVal == "" && colHVal == "")) {
+                    if (!uniqueAgentNames.contains(colHVal))
+                        uniqueAgentNames.add(colHVal);
+                    if (!uniqueAgentIds.contains(colAVal))
+                        uniqueAgentIds.add(colAVal);
+                }
+            }
+            System.out.println("Workbook Open Time: " + App.humanReadableSeconds(workbookOpenTimeInSeconds));
+        }
+
+        System.out.println("# of Unique Agent Names: " + uniqueAgentNames.size());
+        System.out.println("# of Unique Agent Names: " + uniqueAgentIds.size());
+        System.out.println("total number of rows processed: " + totalRowsProcessed);
+        System.out.println("ignored rows: " + ignoredRows);
+        System.out.println("rows that match rule set: " + (totalRowsProcessed - ignoredRows));
+
+        System.out.println("Program End");
+        programDuration = Duration.between(startTime, new Date().toInstant());
+        System.out.println("Program Duration: " + App.humanReadableSeconds(programDuration.getSeconds()));
     }
 }
